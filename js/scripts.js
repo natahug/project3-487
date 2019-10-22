ce45471936750d29ec04f7a46a43da5d9df6735a
natahug <natahug@live.unc.edu>
2019-10-22 00:17:58 -0400

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     $('#e-table').DataTable( {
+//         "ajax": 'data/table.txt',
+
+
+//     } );
+//     // buildChart();
+//     // loadData();
+// } );
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "ajax": "table.txt"
+    } );
+} );
